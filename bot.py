a9a5639b9c5e0cf318c4e52caa45189903985121
PyroUserBot <129763781+PyroUserBot@users.noreply.github.com>
2023-06-08 12:01:33 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,46 +1,92 @@
-from flask import Flask, request, jsonify
-from PIL import Image, ImageDraw, ImageFont
-from pyrogram import idle
+from pyrogram import Client, filters, idle
 
-app = Flask(__name__)
+import requests
 
-@app.route('/welcome_image', methods=['POST'])
-def generate_welcome_image():
-    data = request.get_json()
-    name = data['name']
-    username = data['username']
-    group_name = data['group_name']
+bot_token = "YOUR_BOT_TOKEN"
 
-    # Load the background image
-    background_image = Image.open('IMG_20230601_152627_048.jpg')
+api_id = "16743442"
 
-    # Create a copy of the background image
-    welcome_image = background_image.copy()
+api_hash = "12bbd720f4097ba7713c5e40a11dfd2a"
 
-    # Set the font style and size
-    font = ImageFont.truetype('arial.ttf', 30)
+bot = Client("pet_care_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
 
-    # Create a drawing object
-    draw = ImageDraw.Draw(welcome_image)
+PETFINDER_API_URL = "https://api.petfinder.com/v2"
 
-    # Write the welcome message
-    welcome_message = f"Welcome to {group_name}!"
-    draw.text((50, 50), welcome_message, fill='white', font=font)
+@bot.on_message(filters.command("start"))
 
-    # Write the user details
-    user_details = f"Name: {name}\nUsername: {username}"
-    draw.text((50, 100), user_details, fill='white', font=font)
+def start_command(client, message):
 
-    # Save the generated image
-    welcome_image.save('welcome_image.jpg')
+    client.send_message(message.chat.id, "Welcome to Pet Care Bot! How can I assist you?")
 
-    response = {
-        'message': 'Welcome image generated successfully!',
-        'image_url': 'https://my-flask-app.herokuapp.com/welcome_image.jpg'
-    }
+@bot.on_message(filters.command("petcaretips"))
 
-    return jsonify(response)
+def pet_care_tips_command(client, message):
 
-if __name__ == '__main__':
-    app.run()
-    idle() 
+    client.send_message(message.chat.id, "Sorry, pet care tips are currently unavailable. Please try again later.")
+
+@bot.on_message(filters.command("breedinfo"))
+
+def breed_info_command(client, message):
+
+    if len(message.command) < 2:
+
+        client.send_message(message.chat.id, "Please provide a breed name.")
+
+        return
+
+    breed_name = " ".join(message.command[1:])
+
+    headers = {"Authorization": f"Bearer {ebWoRlapNs1QBu3N65cAhxMU9wzGo1VvicGQUY6Ad0d3NOrxjU}"}
+
+    response = requests.get(f"{PETFINDER_API_URL}/types/dog/breeds?q={breed_name}", headers=headers)
+
+    data = response.json()
+
+    if "breeds" not in data:
+
+        client.send_message(message.chat.id, "Breed information not found.")
+
+        return
+
+    breed = data["breeds"][0]
+
+    breed_info = f"Breed Name: {breed['name']}\n\nDescription: {breed['description']}"
+
+    client.send_message(message.chat.id, breed_info)
+
+@bot.on_message(filters.command("findvet"))
+
+def find_vet_command(client, message):
+
+    if len(message.command) < 2:
+
+        client.send_message(message.chat.id, "Please provide your location.")
+
+        return
+
+    location = " ".join(message.command[1:])
+
+    headers = {"Authorization": f"Bearer {ebWoRlapNs1QBu3N65cAhxMU9wzGo1VvicGQUY6Ad0d3NOrxjU}"}
+
+    response = requests.get(f"{PETFINDER_API_URL}/organizations?type=veterinarian&location={location}", headers=headers)
+
+    data = response.json()
+
+    if "organizations" not in data:
+
+        client.send_message(message.chat.id, "No veterinary services found.")
+
+        return
+
+    organizations = data["organizations"]
+
+    vet_info = "Veterinary Services near you:\n\n"
+
+    for org in organizations:
+
+        vet_info += f"Name: {org['name']}\nLocation: {org['address']['address1']}, {org['address']['city']}\n\n"
+
+    client.send_message(message.chat.id, vet_info)
+
+bot.run()
+idle()
