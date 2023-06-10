0b15aed0c8853362a1bc28bae7a62e7802045f03
PyroUserBot <129763781+PyroUserBot@users.noreply.github.com>
2023-06-10 18:47:05 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 import requests
 
 # Initialize your bot
-bot = Client("accuweather_bot", bot_token="YOUR_TELEGRAM_BOT_TOKEN", api_id=16743442, api_hash="12bbd720f4097ba7713c5e40a11dfd2a")
+bot = Client("accuweather_bot", bot_token="6169875332:AAFgpEnSNbY49ix4Sd1UiRQIbA_jGEhM_ZM", api_id=16743442, api_hash="12bbd720f4097ba7713c5e40a11dfd2a")
 
 # Define the AccuWeather API endpoint and your API key
 base_url = "http://dataservice.accuweather.com"
