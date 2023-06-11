01a9e34a4eeaf80ce4062528d3ba9dd999b3eb89
PyroUserBot <129763781+PyroUserBot@users.noreply.github.com>
2023-06-11 07:50:06 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,17 +4,25 @@ from telegram.ext import Updater, CommandHandler
 TOKEN = "6169875332:AAHkcJvM2V7rB-CTKoi9RHp7hOlDy69RGW4"
 API_KEY = "N3co0uwxaCuE0QG9KgwAaRL9vgor5MKe"
 
+def start(update, context):
+    message = "Welcome to the Weather Bot! Send /weather <city> to get the current weather conditions for a specific city."
+    update.message.reply_text(message)
+
+def help(update, context):
+    message = "This bot provides current weather information for a specific city.\n\nUsage:\n/weather <city> - Get the weather for a specific city."
+    update.message.reply_text(message)
+
 def weather(update, context):
     city = context.args[0]
     url = f"http://dataservice.accuweather.com/locations/v1/cities/search?apikey={API_KEY}&q={city}"
     response = requests.get(url).json()
     
-    if response:
+    if response and len(response) > 0:
         location_key = response[0]['Key']
         conditions_url = f"http://dataservice.accuweather.com/currentconditions/v1/{location_key}?apikey={API_KEY}"
         conditions_response = requests.get(conditions_url).json()
         
-        if conditions_response:
+        if conditions_response and len(conditions_response) > 0:
             try:
                 temperature = conditions_response[0]["Temperature"]["Metric"]["Value"]
                 weather_text = conditions_response[0]["WeatherText"]
@@ -34,6 +42,8 @@ def weather(update, context):
 def main():
     updater = Updater(token=TOKEN, use_context=True)
     dispatcher = updater.dispatcher
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(CommandHandler("help", help))
     dispatcher.add_handler(CommandHandler("weather", weather))
     updater.start_polling()
     updater.idle()
