a2dc19461f15503aa2c2734e441b115ddad7107f
PyroUserBot <129763781+PyroUserBot@users.noreply.github.com>
2023-06-11 17:19:55 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,57 +1,77 @@
 import requests
-from telegram.ext import Updater, CommandHandler
+from pyrogram import Client, filters, idle
+from pyrogram.types import InputMediaPhoto
+from telegram import ReplyKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
-TOKEN = "6169875332:AAHkcJvM2V7rB-CTKoi9RHp7hOlDy69RGW4"
-API_KEY = "N3co0uwxaCuE0QG9KgwAaRL9vgor5MKe"
+# AccuWeather API key
+API_KEY = 'your_accuweather_api_key'
 
+# Telegram bot token
+TOKEN = 'your_telegram_bot_token'
+
+# API ID and API hash for Pyrogram
+API_ID = '16743442'
+API_HASH = '12bbd720f4097ba7713c5e40a11dfd2a'
+
+# Pyrogram client
+app = Client(
+    'my_bot',
+    bot_token=TOKEN,
+    api_id=API_ID,
+    api_hash=API_HASH
+)
+
+# Telegram bot
+bot = Updater(TOKEN, use_context=True)
+dispatcher = bot.dispatcher
+
+# Command handler
 def start(update, context):
-    message = "Welcome to the Weather Bot! Send /weather <city> to get the current weather conditions for a specific city."
-    update.message.reply_text(message)
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Welcome to the Weather Bot!")
+    context.bot.send_chat_action(chat_id=update.effective_chat.id, action="upload_photo")
+    context.bot.send_media_group(
+        chat_id=update.effective_chat.id,
+        media=[
+            InputMediaPhoto(media="https://example.com/welcome_image.jpg", caption="Welcome to the Weather Bot!")
+        ]
+    )
 
-def help(update, context):
-    message = "This bot provides current weather information for a specific city.\n\nUsage:\n/weather <city> - Get the weather for a specific city."
-    update.message.reply_text(message)
+start_handler = CommandHandler('start', start)
+dispatcher.add_handler(start_handler)
 
-def weather(update, context):
-    city = context.args[0]
+# Weather handler
+def get_weather(update, context):
+    city = update.message.text
+
+    # Retrieve weather data
     url = f"http://dataservice.accuweather.com/locations/v1/cities/search?apikey={API_KEY}&q={city}"
     response = requests.get(url).json()
-    
-    if response and len(response) > 0:
-        if 'Key' in response[0]:
-            location_key = response[0]['Key']
-            conditions_url = f"http://dataservice.accuweather.com/currentconditions/v1/{location_key}?apikey={API_KEY}"
-            conditions_response = requests.get(conditions_url).json()
-            
-            if conditions_response and len(conditions_response) > 0:
-                try:
-                    temperature = conditions_response[0]["Temperature"]["Metric"]["Value"]
-                    weather_text = conditions_response[0]["WeatherText"]
-                    humidity = conditions_response[0].get("RelativeHumidity")
-                    wind_speed = conditions_response[0]["Wind"]["Speed"]["Metric"]["Value"]
-                    wind_direction = conditions_response[0]["Wind"]["Direction"]["Localized"]
-                    
-                    message = f"Weather in {city}:\n\nTemperature: {temperature}°C\nWeather: {weather_text}\nHumidity: {humidity}%\nWind Speed: {wind_speed} km/h\nWind Direction: {wind_direction}"
-                    update.message.reply_text(message)
-                except KeyError:
-                    update.message.reply_text("Sorry, I couldn't retrieve the weather information for that location.")
-            else:
-                update.message.reply_text("Sorry, I couldn't retrieve the weather information for that location.")
-        else:
-            update.message.reply_text("Sorry, I couldn't find the specified location.")
-    else:
-        update.message.reply_text("Sorry, I couldn't find the specified location.")
+    location_key = response[0]['Key']
 
+    url = f"http://dataservice.accuweather.com/forecasts/v1/daily/5day/{location_key}?apikey={API_KEY}&metric=true"
+    response = requests.get(url).json()
+    forecasts = response['DailyForecasts']
 
+    # Format weather information
+    message = f"Weather forecast for {city}:\n\n"
+    for forecast in forecasts:
+        date = forecast['Date']
+        min_temp = forecast['Temperature']['Minimum']['Value']
+        max_temp = forecast['Temperature']['Maximum']['Value']
+        day_desc = forecast['Day']['IconPhrase']
+        night_desc = forecast['Night']['IconPhrase']
+        message += f"Date: {date}\nMin Temp: {min_temp}°C\nMax Temp: {max_temp}°C\nDay: {day_desc}\nNight: {night_desc}\n\n"
 
-def main():
-    updater = Updater(token=TOKEN, use_context=True)
-    dispatcher = updater.dispatcher
-    dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(CommandHandler("help", help))
-    dispatcher.add_handler(CommandHandler("weather", weather))
-    updater.start_polling()
-    updater.idle()
+    context.bot.send_message(chat_id=update.effective_chat.id, text=message)
 
-if __name__ == '__main__':
-    main()
+weather_handler = MessageHandler(Filters.text & (~Filters.command), get_weather)
+dispatcher.add_handler(weather_handler)
+
+# Start the bot
+bot.start_polling()
+bot.idle()
+
+# Start the Pyrogram client
+app.run()
+idle()
