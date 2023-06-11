271d4c443d2f868d481a3da06e43876a9198c268
PyroUserBot <129763781+PyroUserBot@users.noreply.github.com>
2023-06-11 07:53:31 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -18,26 +18,30 @@ def weather(update, context):
     response = requests.get(url).json()
     
     if response and len(response) > 0:
-        location_key = response[0]['Key']
-        conditions_url = f"http://dataservice.accuweather.com/currentconditions/v1/{location_key}?apikey={API_KEY}"
-        conditions_response = requests.get(conditions_url).json()
-        
-        if conditions_response and len(conditions_response) > 0:
-            try:
-                temperature = conditions_response[0]["Temperature"]["Metric"]["Value"]
-                weather_text = conditions_response[0]["WeatherText"]
-                humidity = conditions_response[0]["RelativeHumidity"]
-                wind_speed = conditions_response[0]["Wind"]["Speed"]["Metric"]["Value"]
-                wind_direction = conditions_response[0]["Wind"]["Direction"]["Localized"]
-                
-                message = f"Weather in {city}:\n\nTemperature: {temperature}°C\nWeather: {weather_text}\nHumidity: {humidity}%\nWind Speed: {wind_speed} km/h\nWind Direction: {wind_direction}"
-                update.message.reply_text(message)
-            except KeyError:
+        if 'Key' in response[0]:
+            location_key = response[0]['Key']
+            conditions_url = f"http://dataservice.accuweather.com/currentconditions/v1/{location_key}?apikey={API_KEY}"
+            conditions_response = requests.get(conditions_url).json()
+            
+            if conditions_response and len(conditions_response) > 0:
+                try:
+                    temperature = conditions_response[0]["Temperature"]["Metric"]["Value"]
+                    weather_text = conditions_response[0]["WeatherText"]
+                    humidity = conditions_response[0]["RelativeHumidity"]
+                    wind_speed = conditions_response[0]["Wind"]["Speed"]["Metric"]["Value"]
+                    wind_direction = conditions_response[0]["Wind"]["Direction"]["Localized"]
+                    
+                    message = f"Weather in {city}:\n\nTemperature: {temperature}°C\nWeather: {weather_text}\nHumidity: {humidity}%\nWind Speed: {wind_speed} km/h\nWind Direction: {wind_direction}"
+                    update.message.reply_text(message)
+                except KeyError:
+                    update.message.reply_text("Sorry, I couldn't retrieve the weather information for that location.")
+            else:
                 update.message.reply_text("Sorry, I couldn't retrieve the weather information for that location.")
         else:
-            update.message.reply_text("Sorry, I couldn't retrieve the weather information for that location.")
+            update.message.reply_text("Sorry, I couldn't find the specified location.")
     else:
         update.message.reply_text("Sorry, I couldn't find the specified location.")
+
 
 def main():
     updater = Updater(token=TOKEN, use_context=True)
